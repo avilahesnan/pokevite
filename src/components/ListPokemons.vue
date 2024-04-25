deeabd7b71de0a11401f272602a0edcec6ac570f
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-25 16:49:33 -0300

    feat: add connect pokeapi

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-4">
+        <div class="card p-2 mb-3">
+            <p class="text-center">{{ pokemon.name }}</p>
+            <img
+                class="card-img-top"
+                height="80px"
+                alt="pokemon_image"
+                :src="urlBaseSvg">
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const pokemon = defineProps(['name', 'urlBaseSvg'])
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <div class="container">
+            <div class="col-sm-12 col-md-6">
+                <div class="card">
+                    <div class="card-body row">
+                        <ListPokemons
+                            v-for="pokemon in pokemons"
+                            :key="pokemon.name"
+                            :name="pokemon.name"
+                            :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<script setup>
+
+import { onMounted, reactive, ref} from 'vue'
+
+import ListPokemons from '../components/ListPokemons.vue'
+
+let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
+let pokemons = reactive(ref())
+
+onMounted(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=494&offset=0')
+        .then(res => res.json())
+        .then(res => pokemons.value = res.results)
+})
+
+</script>
